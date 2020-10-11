0aac3d2943ad20eeb56b9466f07456663eb13e9c
NeverySleeps <riftingwolf@gmail.com>
2020-10-11 17:20:20 +0100

    reward 1 point for every 100 xp gained

--- cogs/level.py
+++ cogs/level.py
@@ -15,12 +15,12 @@ from helpers.utils import min_level
 from api_key import userColl
 import pymongo
 
+
 class level(commands.Cog, name="levelling"):
     def __init__(self, bot, hidden):
         self.hidden = hidden
         self.bot = bot
         self.update_multipliers()
-
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
@@ -59,36 +59,22 @@ class level(commands.Cog, name="levelling"):
             json.dump(config, f)
         self.update_multipliers()
 
-    @commands.Cog.listener()  
+    @commands.Cog.listener()
     async def on_message(self, message):
         if message.author.bot:
             return
         if message.author.id == 307468800125829120:
             return
-        # with open("users.json", "r") as f:
-        #     users = json.load(f)
-        # if message.mentions and message.guild.me in message.mentions:
-        #     await message.channel.send("WHY PING")
         if not message.guild:
             return
         else:
             userData = await self.update_data(message.author)
-            # if message.attachments:
-            #     number =  30
-            # elif len("".join(message.content)) > 150:
-            #     number = 50
-            # else:
-            #     number = 25
             if str(message.channel.id) in self.multipliers:
                 multiplier = self.multipliers[str(message.channel.id)]
             else:
                 multiplier = 1
-            # if message.channel.id == 685782425502220311:
-            #     multiplier = 0.5
-            # else:
-            #     multiplier = 1
             if message.attachments:
-                number =  math.trunc(30 * multiplier)
+                number = math.trunc(30 * multiplier)
             elif len("".join(message.content)) > 150:
                 number = math.trunc(50 * multiplier)
             else:
@@ -98,22 +84,26 @@ class level(commands.Cog, name="levelling"):
 
     async def update_data(self, user):
         if not await userColl.find_one({"_id": str(user.id)}):
-            await userColl.insert_one({"_id": str(user.id), "experience": 0, "weekly": 0, "level": 1, "last_message": 0})
+            await userColl.insert_one(
+                {"_id": str(user.id), "experience": 0, "weekly": 0, "level": 1, "last_message": 0})
         return await userColl.find_one({"_id": str(user.id)})
-
 
     async def add_experience(self, userData, user, exp):
         if time.time() - userData["last_message"] > 30:
-            await userColl.update_one({"_id": str(user.id)}, {"$inc": {"experience": exp, "weekly": exp}, "$set": {"last_message": time.time()}})
+            points_bonus = 1 if userData["experience"] > userData["last_points"] + 100 else 0
+            await userColl.update_one({"_id": str(user.id)},
+                                      {"$inc": {"experience": exp, "weekly": exp, "points": points_bonus},
+                                       "$set": {"last_message": time.time(), "last_points": userData["experience"] + exp if points_bonus else userData["last_points"]}})
             return await userColl.find_one({"_id": str(user.id)})
         else:
             return await userColl.find_one({"_id": str(user.id)})
+
     async def level_up(self, ctx, userData, user):
         experience = userData["experience"]
         lvl_start = userData["level"]
         lvl_end = 50 * (lvl_start ** 1.5)
         if experience > lvl_end:
-            await userColl.update_one({"_id": str(user.id)}, {"$inc": {"level": 1}, "$set": {"experience": 0}})
+            await userColl.update_one({"_id": str(user.id)}, {"$inc": {"level": 1}, "$set": {"experience": 0, "last_points": 0}})
             await ctx.channel.send(f":tada: Congrats {user.mention}, you levelled up to level {lvl_start + 1}!")
             with open('levelroles.json') as f:
                 levelroles = json.load(f)
@@ -121,18 +111,19 @@ class level(commands.Cog, name="levelling"):
                 role = ctx.guild.get_role(int(levelroles[str(lvl_start + 1)]))
                 await user.add_roles(role)
 
-    @commands.command(aliases = ['level'])
-    async def rank(self, ctx, user:discord.Member=None):
+    @commands.command(aliases=['level'])
+    async def rank(self, ctx, user: discord.Member = None):
         """View your or the mentioned user's level"""
         if not user:
             user = ctx.author
         userData = await userColl.find_one({"_id": str(user.id)})
         if not userData:
             return await ctx.send("This user has no level")
-        embed = discord.Embed(title=f"Level: {str(round(userData['level']))}", description=f"XP: {str(round(userData['experience']))}/{str(round(50 * (round(userData['level']) ** 1.5)))}\nWeekly XP: {str(round(userData['weekly']))}", color=0x00FF00)
+        embed = discord.Embed(title=f"Level: {str(round(userData['level']))}",
+                              description=f"XP: {str(round(userData['experience']))}/{str(round(50 * (round(userData['level']) ** 1.5)))}\nWeekly XP: {str(round(userData['weekly']))}",
+                              color=0x00FF00)
         embed.set_author(name=user, icon_url=user.avatar_url)
         await ctx.send(embed=embed)
-
 
     @commands.command()
     async def weekly(self, ctx):
@@ -153,7 +144,7 @@ class level(commands.Cog, name="levelling"):
                     contents[embedcount].set_footer(text=f"page {embedcount + 1} of 15")
                     embedcount += 1
                     if embedcount < 15:
-                        contents.append(discord.Embed(color=0x00FF00))     
+                        contents.append(discord.Embed(color=0x00FF00))
                         contents[embedcount].set_author(name="Nullzee's cave leaderboard", icon_url=ctx.guild.icon_url)
                     string = ''
             else:
@@ -179,8 +170,10 @@ class level(commands.Cog, name="levelling"):
             users["config"]["week_start"] = math.trunc(time.time())
             with open('users.json', 'w') as f:
                 json.dump(users, f)
-            await self.bot.get_guild(667953033929293855).get_channel(667957285837864960).send(embed=discord.Embed(description="Weekly XP leaderboard was reset"), color=discord.Color.blue())
-            await ctx.send(embed=discord.Embed(description="Weekly XP leaderboard was reset", color=discord.Color.blue()))
+            await self.bot.get_guild(667953033929293855).get_channel(667957285837864960).send(
+                embed=discord.Embed(description="Weekly XP leaderboard was reset"), color=discord.Color.blue())
+            await ctx.send(
+                embed=discord.Embed(description="Weekly XP leaderboard was reset", color=discord.Color.blue()))
 
     # @tasks.loop(hours=1)
     # async def weeklyreset(self):
@@ -197,12 +190,6 @@ class level(commands.Cog, name="levelling"):
     #             json.dump(users, f)
     #         await self.bot.get_guild(667953033929293855).get_channel(667957285837864960).send(embed=discord.Embed(description="Weekly XP leaderboard was reset"), color=discord.Color.blue())
 
-        
-
-
-
-
-
     @commands.command(hidden=True)
     @commands.has_guild_permissions(manage_messages=True)
     async def levelBackup(self, ctx):
@@ -216,11 +203,9 @@ class level(commands.Cog, name="levelling"):
             json.dump(users, f)
         await ctx.send("Backup created")
 
-
-
     @commands.command(hidden=True)
     @commands.has_guild_permissions(manage_messages=True)
-    async def removeweekly(self, ctx, user:discord.Member, xp:int):
+    async def removeweekly(self, ctx, user: discord.Member, xp: int):
         if 667953757954244628 in [z.id for z in user.roles]:
             return await ctx.send("Cannot remove XP from that user")
         if xp < 0:
@@ -231,10 +216,9 @@ class level(commands.Cog, name="levelling"):
             await userColl.update_one({"_id": str(user.id)}, {"$inc": {"weekly": -xp}})
         await ctx.send(f"removed {xp} weekly xp from {user.mention}")
 
-
     @commands.command(hidden=True)
     @commands.has_guild_permissions(manage_messages=True)
-    async def removeXP(self, ctx, user:discord.Member, xp:int):
+    async def removeXP(self, ctx, user: discord.Member, xp: int):
         if 667953757954244628 in [z.id for z in user.roles]:
             return await ctx.send("Cannot remove XP from that user")
         if xp < 0:
@@ -244,10 +228,6 @@ class level(commands.Cog, name="levelling"):
         else:
             await userColl.update_one({"_id": str(user.id)}, {"$inc": {"experience": -xp}})
         await ctx.send(f"removed {xp} xp from {user.mention}")
-
-
-
-
 
     @commands.command()
     async def leaderboard(self, ctx):
@@ -268,7 +248,7 @@ class level(commands.Cog, name="levelling"):
                     contents[embedcount].set_footer(text=f"page {embedcount + 1} of 15")
                     embedcount += 1
                     if embedcount < 15:
-                        contents.append(discord.Embed(color=0x00FF00))     
+                        contents.append(discord.Embed(color=0x00FF00))
                         contents[embedcount].set_author(name="Nullzee's cave leaderboard", icon_url=ctx.guild.icon_url)
                     string = ''
             else:
@@ -278,8 +258,6 @@ class level(commands.Cog, name="levelling"):
                 pages = Paginator(self.bot, msg, embeds=contents, timeout=180, use_more=True, only=ctx.author)
                 await pages.start()
                 return
-
-
 
 
 def setup(bot):
