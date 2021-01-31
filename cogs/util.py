4412c29a1b2647ec46db7fdb89e832e55ec3e293
aoieop <49662700+aoieop@users.noreply.github.com>
2021-01-31 18:14:31 -0500

    made -nitro a bit more accurate :/

--- cogs/util.py
+++ cogs/util.py
@@ -163,6 +163,8 @@ class util(commands.Cog, name="Other"):
 - A message posted in <#714073835791712267>
 - The Nitro Booster (rich) role
 - Role is positioned under @Null
+- Access to all level restricted commands
+- Access to all giveaways
 """, inline=False)
         embed.set_thumbnail(url="https://images-ext-1.discordapp.net/external/PbC_AHw6x6OR_5a6hpvuLTP6nBEnpc5e-ftbgOx9oks/https/i.ytimg.com/vi/ZyX7U78keu0/maxresdefault.jpg?width=960&height=540")
         await ctx.send(embed=embed)
