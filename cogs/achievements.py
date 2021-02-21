c3aa29f791f7c1d2e8dd9fb1177951c1bfc11019
aoieop <austincui2007@gmail.com>
2021-02-21 13:32:39 -0500

    achievement UI

--- cogs/achievements.py
+++ cogs/achievements.py
@@ -1,7 +1,9 @@
 from discord.ext import commands
 import discord
-from perks import achievements
+from perks.achievements import achievements
 from helpers.events import Emitter
+from PIL import Image, ImageDraw, ImageFont
+from math import ceil
 
 
 class ShallowContext:
@@ -20,6 +22,40 @@ class ShallowContext:
 
     async def send(self, *args, **kwargs):
         return await self.channel.send(*args, **kwargs)
+
+font_0 = ImageFont.truetype('Roboto-Medium.ttf', 20)
+font_1 = ImageFont.truetype('Roboto-thin.ttf', 15)
+
+names = []
+descriptions = []
+for achievement in achievements:
+    names.append(achievement)
+    descriptions.append(achievements[achievement]["description"])
+
+def achievement_box(image, x: int, y: int, name: str, definition: str):
+    draw = ImageDraw.Draw(image)
+    draw.rectangle([(x, y), (x + 300, y + 100)], 'white', 'black')
+    draw.text((x + 10, y + 10), name, 'black', font=font_0)
+    draw.text((x + 10, y + 40), definition, 'black', font=font_1)
+
+def addPage(image, page: int, repeat: int):
+    xPos = 100
+    yPos = 100
+    loop = range(0, repeat)
+    for i in loop:
+        pageNum = ((page - 1) * 3) + i
+        achievement_box(image, xPos, yPos, names[pageNum], descriptions[pageNum])
+        yPos += 150
+
+def achievement_page(page):
+    lastPage = ceil(len(achievements) / 3)
+    mod = len(achievements) % 3
+    image = Image.new('RGBA', (500, 600), (0, 0, 0, 0))
+    if page == lastPage:
+        addPage(image, page, mod)
+    else:
+        addPage(image, page, 3)
+    return image.save('image.png', format='PNG')
 
 class Achievements(commands.Cog):
 
@@ -40,6 +76,11 @@ class Achievements(commands.Cog):
     @commands.Cog.listener()
     async def on_command(self, ctx):
         await self.emitter.emit("command", ctx, ctx.command.name)
+
+    @commands.command()
+    async def achievement(self, ctx, page: int):
+        achievement_page(page)
+        await ctx.send(file=discord.File("image.png"))
 
 
 def setup(bot):
