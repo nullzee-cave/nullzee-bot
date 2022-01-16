10cc3f82488b1630af0a3a669b2e73321cd08125
GoobyDev <70377808+CodeyArch@users.noreply.github.com>
2022-01-16 14:58:57 +0000

    Added more useless commands, separated slap and stab
    Added -mistake, -gn, -gm and separated the slap and stab commands as they are not at all the same command

--- cogs/useless_commands.py
+++ cogs/useless_commands.py
@@ -13,6 +13,11 @@ class Useless_commands(commands.Cog, name='Useless Commands'):
         embed = await Embed(ctx.author, description = f'{ctx.author.mention} shakes their head').user_colour()
         await ctx.send(embed = embed)
         
+    @commands.command(aliases = ["failed"])
+    async def mistake(self, ctx):
+        embed = await Embed(ctx.author, description = f'{ctx.author.mention} had an <a:sparkles:932286365008281640> Unintentional failure <a:sparkles:932286365008281640>').user_colour()
+        await ctx.send(embed = embed)
+        
     @commands.command()
     async def f(self, ctx):
         embed = await Embed(ctx.author, description = f'<:press_f:709688246774267905> {ctx.author.mention} has paid their respects').user_colour()
@@ -37,7 +42,27 @@ class Useless_commands(commands.Cog, name='Useless Commands'):
             embed = await Embed(ctx.author, description = f"{ctx.author.mention} hugged {member.mention}", url ="https://cdn.discordapp.com/emojis/759485870146584586.png?7v=1").user_colour()
             await ctx.send(embed = embed)
     
-    @commands.command(aliases = ["slap"])
+    @commands.command()
+    async def gn(self,ctx,member: discord.Member):
+        if member == ctx.author:
+            embed = await Embed(ctx.author, description = f"{ctx.author} wished themselves a good sleep",
+                                               url = "https://cdn.discordapp.com/emojis/759485870146584586.png?v=1", color=0x00FF00).set_footer(text="Goodnight!").user_colour()
+            await ctx.send(embed = embed)
+        else:    
+            embed = await Embed(ctx.author, description = f"{ctx.author.mention} wished {member.mention} happy dreams", url ="https://cdn.discordapp.com/emojis/759485870146584586.png?7v=1").user_colour()
+            await ctx.send(embed = embed)
+    
+    @commands.command()
+    async def gm(self,ctx,member: discord.Member):
+        if member == ctx.author:
+            embed = await Embed(ctx.author, description = f"{ctx.author} wished themselves a good morning",
+                                               url = "https://cdn.discordapp.com/emojis/759485870146584586.png?v=1", color=0x00FF00).set_footer(text="Good morning!").user_colour()
+            await ctx.send(embed = embed)
+        else:    
+            embed = await Embed(ctx.author, description = f"{ctx.author.mention} blessed {member.mention} with a good morning!", url ="https://cdn.discordapp.com/emojis/759485870146584586.png?7v=1").user_colour()
+            await ctx.send(embed = embed)
+    
+    @commands.command()
     async def stab(self, ctx, member: discord.Member):
         gamer = ctx.author.mention
         stabby = member.mention
@@ -53,18 +78,27 @@ class Useless_commands(commands.Cog, name='Useless Commands'):
                  f"{stabby} got stabby stabby stabbed by {gamer}",
                  f"{gamer} :knife: {stabby}",
                  f"{stabby} wasn't spicy enough for {gamer}",
-                 f"{stabby} was brutally slain by {gamer}",
-                 f"{stabby} was slapped by {gamer}",
-                 f"{gamer} slapped {stabby}",
-                 f"{gamer} smacked {stabby} across the face",
-                 f"{gamer} hurt {stabby}"]
+                 f"{stabby} was brutally slain by {gamer}"]
         if member ==  ctx.author:
             embed = await Embed(ctx.author, description = "We do not promote self harm in Nullzee's Cave.").user_colour()
         else:
             embed = await Embed(ctx.author, description = str(random.choice(funny))).user_colour()
         await ctx.send(embed = embed)
-             
-
+    
+    @commands.command()
+    async def slap(self, ctx, member: discord.Member):
+        gamer = ctx.author.mention
+        slappy = member.mention
+        funny = [f"{slappy} was slapped by {gamer}",
+                 f"{gamer} slapped {slappy}",
+                 f"{gamer} smacked {slappy} across the face",
+                 f"{gamer} hurt {slappy}"]
+        if member ==  ctx.author:
+            embed = await Embed(ctx.author, description = "We do not promote self harm in Nullzee's Cave.").user_colour()
+        else:
+            embed = await Embed(ctx.author, description = str(random.choice(funny))).user_colour()
+        await ctx.send(embed = embed)
+    
     @commands.command()
     async def bonk(self, ctx, member: discord.Member):
         embed = await Embed(ctx.author, description = f"{member.mention} got bonked").user_colour()
