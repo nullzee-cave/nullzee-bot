3402e53c43029ae1965d3a5fa5f57f776d41ebf9
Natalia Spence <52349324+Septikai@users.noreply.github.com>
2022-06-22 18:59:51 +0100

    create template for api_key

---
+++ api_key_template.py
@@ -0,0 +1,22 @@
+CONNECTION_STRING = ""
+
+DEV_ID = 0
+TOKEN = ""
+HYPIXEL_API_KEY = ""
+YT_API_KEY = ""
+PREFIX = "-"
+
+COGS = [
+        "cogs.achievements",
+        "cogs.automod",
+        "cogs.events_v2",
+        "cogs.giveaway",
+        "cogs.level",
+        "cogs.logging",
+        "cogs.moderation",
+        "cogs.points",
+        "cogs.staff",
+        "cogs.tickets",
+        "cogs.useless_commands",
+        "cogs.util",
+        ]
