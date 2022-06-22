a36dd00e6c810a2b3116a0b0b2ea023a279dc56a
Kian Spence <52349324+Septikai@users.noreply.github.com>
2022-06-22 19:03:13 +0100

    add comments for template

--- api_key_template.py
+++ api_key_template.py
@@ -1,11 +1,25 @@
+# MongoDB Connection String
+# Allows the bot to connect to the database
 CONNECTION_STRING = ""
 
+# Developer's Discord ID
+# Used for various small things, such as sending messages when something breaks
 DEV_ID = 0
+# The Bot Token
+# This is the token which tells the code which bot to run on
 TOKEN = ""
+# A Hypixel API Key
+# Used to fetch data on which roles should be given by the `-claimroles` command
 HYPIXEL_API_KEY = ""
+# A YouTube API Key
+# Used to fetch Nullzee's subscriber count
 YT_API_KEY = ""
+# The Bot Prefix
+# Used for message commands
 PREFIX = "-"
 
+# Cogs
+# All cogs in this list will be loaded on startup
 COGS = [
         "cogs.achievements",
         "cogs.automod",
